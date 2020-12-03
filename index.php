cf90f81d9594f3c15c18ad63915f2248bf86699a
moe akimoto <akimoe0503@gmail.com>
2020-12-03 17:01:29 +0900

    クローン作成

---
+++ index.php
@@ -0,0 +1,213 @@
+<?php get_header(); ?>
+
+     
+     <!-- ------------------- ここからtopphoto ---------------------->
+     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/0.23.47.jpg">
+        </div>
+          <div class="carousel-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/a53d9b042e70.jpg">
+        </div>
+          <div class="carousel-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/0.10.17.jpg">
+        </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+     
+     <!--------------------- ここからnew allival ---------------------->
+     <p><strong>NEW ALLIVAL</strong></p>
+     <div class="swiper-container" >
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/newimg(1).jpg" alt=""></div>
+            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/428 (4).jpg" alt=""></div>
+            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/newimg.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/428.jpg" alt=""></div>            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/newimg(2).jpg" alt=""></div>
+            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/newimg(3).jpg" alt=""></div>
+            <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/newimg(4).jpg" alt=""></div>
+
+        </div>
+        <!-- If we need pagination -->
+        <div class="swiper-pagination"></div>
+        
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+    </div>
+
+        <!-- for iphone -->
+    <div id="carousel-newallive" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/newimg(1).jpg">
+          </div>
+          <div class="carousel-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/428 (4).jpg">          
+          </div>
+          <div class="carousel-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/newimg.jpg">          
+          </div>
+        <div class="carousel-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/428.jpg">
+        </div>
+        <div class="carousel-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/newimg(3).jpg">          
+        </div>
+        <div class="carousel-item">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/newimg(4).jpg">          
+        </div>
+        <a class="carousel-control-prev" href="#carousel-newallive" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carousel-newallive" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+     <!--------------------- ここから news/event---------------------->
+     <p><strong>EVENT・NEWS</strong></p>
+     <div class="swiper-container" id="in">
+      <!-- Additional required wrapper -->
+      <div class="swiper-wrapper">
+          <!-- Slides -->
+          <div class="swiper-slide" id="in2"><img src="<?php echo get_template_directory_uri(); ?>/img/top.jpg" alt=""></div>
+          <div class="swiper-slide" id="in2"><img src="<?php echo get_template_directory_uri(); ?>/img/top1.jpg" alt=""></div>
+          <div class="swiper-slide" id="in2"><img src="<?php echo get_template_directory_uri(); ?>/img/top2.jpg" alt=""></div>
+          <div class="swiper-slide" id="in2"><img src="<?php echo get_template_directory_uri(); ?>/img/top3.jpg" alt=""></div>
+      </div>
+      <!-- If we need pagination -->
+      <div class="swiper-pagination"></div>
+      
+      <!-- If we need navigation buttons -->
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>
+  </div>
+
+            <!-- for iphone -->
+          <div id="carousel-news" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/top.jpg">
+                </div>
+                <div class="carousel-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/top1.jpg">          
+                </div>
+                <div class="carousel-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/top2.jpg">          
+                </div>
+                <div class="carousel-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/top3.jpg">
+                </div>
+
+                <a class="carousel-control-prev" href="#carousel-news" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carousel-news" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+                </a>
+            </div>
+          </div>
+
+ <!--------------------- ここから tab menu---------------------->
+ 
+ <p><strong>ABOUT ITEMS</strong></p>
+
+ <div class="margin-left">
+  <ul class="tab-nav">
+    <li class="main-color"><a href="#tab01" class="active">NEW color</a></li>
+    <li class="in-color"><a href="#tab02">NEW Items</a></li>
+    <li class="out-color"><a href="#tab03">NEW Lips</a></li>
+  </ul>
+
+   <div class="tab-content">
+      <div id="tab01" class="active">
+        <div class="in-article">唇をよりキレイに見せる魅力的な<strong>MLBB</strong>カラーで
+          潤いのある滑らかなテクスチャのティントはまるで濃密なシロップで包んだかの様、
+          時間が経つにつれさらに<strong>凸凹</strong>になめらかに密着し落ちにくい、きらめくグロスリップに仕上げます。また、べたつかず潤いが残る<br>しっとりとしたテクスチャーです。
+          </div>
+         <img src="<?php echo get_template_directory_uri(); ?>/img/Lips.jpg" alt="">
+      </div>
+      <div id="tab02">
+        <div class="in-article">肌に馴染みやすく使いやすい配色で、滑らかで使い心地のいいアイシャドウ。マットカラーとグリッターで上品にもクールにも。美容系<strong>Youtuber</strong>アカウントから生まれた立ち上げからわずか<strong>2</strong>年で、<strong>2018</strong>年韓国のブランドアワードで賞を取ったブランド<br>「ロムアンド」の人気商品です。
+        </div>
+           <img src="<?php echo get_template_directory_uri(); ?>/img/new.jpg" alt="">
+      </div>
+      <div id="tab03">
+        <div class="in-article">唇に水の膜を張ったような透明な光沢と<strong>カラー</strong>の密着度の高い新感覚のティント。
+          カラーとツヤコーティング層の二層で、しっかりと色付けした後にツヤでコーティングし光沢感溢れる唇に仕上がります。
+          塗った瞬間から色が吸収されるようにしっかり馴染み<br>　<strong>3秒</strong>待つとツヤ感が浮き出ます。</div>
+           <img src="<?php echo get_template_directory_uri(); ?>/img/lips2.jpg" alt="">
+      </div>
+  </div>
+</div>
+
+
+     <!--------------------- ここから best seller---------------------->
+     <p><strong>BEST SELLER</strong></p>
+     <div class="swiper-container">
+      <!-- Additional required wrapper -->
+      <div class="swiper-wrapper">
+          <!-- Slides -->
+          <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/cosme.jpg" alt=""></div>
+          <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/cosme2.jpg" alt=""></div>
+          <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/cosme3.jpg" alt=""></div>
+          <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/cosme7.png" alt=""></div>
+          <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/img/cosme9.jpg" alt=""></div>
+      </div>
+      <!-- If we need pagination -->
+      <div class="swiper-pagination"></div>
+      
+      <!-- If we need navigation buttons -->
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>
+  </div>
+
+          <!-- for iphone -->
+          <div id="carousel-bestseller" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/cosme.jpg">
+                </div>
+                <div class="carousel-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/cosme2.jpg">          
+                </div>
+                <div class="carousel-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/cosme3.jpg">          
+                </div>
+                <div class="carousel-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/cosme9.jpg">          
+                </div>
+
+                <a class="carousel-control-prev" href="#carousel-bestseller" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carousel-bestseller" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+                </a>
+            </div>
+          </div>
+
+
+<?php get_footer(); ?>
